13fc6483e6c0d271e7f93a4f094d4d9ddc200095
Aldo Pizarro <a.pizarro339@gmail.com>
2019-07-05 15:07:34 -0400

    first

--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,15 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const session = require('koa-session');
-const passport = require('koa-passport');
-
-const indexRoutes = require('./routes/index');
-const movieRoutes = require('./routes/movies');
-const authRoutes = require('./routes/auth');
-const store = require('./session');
 
 const app = new Koa();
-const PORT = process.env.PORT || 1337;
+const PORT = 1337;
 
-// sessions
-app.keys = ['super-secret-key'];
-app.use(session({ store }, app));
+app.use(async (ctx) => {
+  ctx.body = {
+    status: 'success',
+    message: 'hello, world!'
+  };
+});
 
-// body parser
-app.use(bodyParser());
-
-// authentication
-require('./auth');
-app.use(passport.initialize());
-app.use(passport.session());
-
-// routes
-app.use(indexRoutes.routes());
-app.use(movieRoutes.routes());
-app.use(authRoutes.routes());
-
-// server
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
