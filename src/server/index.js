7bb92ee5c4e38dd4c1715c3133f89c8022dc4e30
Aldo Pizarro <a.pizarro339@gmail.com>
2019-07-09 12:24:45 -0400

    asd

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,63 @@
-const Koa = require('koa');
+const Express = require("express");
+const Mongoose = require("mongoose");
+const BodyParser = require("body-parser");
 
-const app = new Koa();
-const PORT = 1337;
+var app = Express();
 
-app.use(async (ctx) => {
-  ctx.body = {
-    status: 'success',
-    message: 'hello, world!'
-  };
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+Mongoose.connect("mongodb://localhost:27017/ExampleNodeMongo");
+
+const PersonModel = Mongoose.model("personas", {
+  firstname: String,
+  lastname: String
 });
 
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+app.post("/person", async (request, response) => {
+  try {
+    var person = new PersonModel(request.body);
+    var result = await person.save();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+app.get("/people", async (request, response) => {
+  try {
+      var result = await PersonModel.find().exec();
+      response.send(result);
+  } catch (error) {
+      response.status(500).send(error);
+  }
+});
+app.get("/person/:id", async (request, response) => {
+  try {
+      var person = await PersonModel.findById(request.params.id).exec();
+      response.send(person);
+  } catch (error) {
+      response.status(500).send(error);
+  }
+});
+app.put("/person/:id", async (request, response) => {
+  try {
+      var person = await PersonModel.findById(request.params.id).exec();
+      person.set(request.body);
+      var result = await person.save();
+      response.send(result);
+  } catch (error) {
+      response.status(500).send(error);
+  }
+});
+app.delete("/person/:id", async (request, response) => {
+  try {
+      var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+      response.send(result);
+  } catch (error) {
+      response.status(500).send(error);
+  }
 });
 
-module.exports = server;
+app.listen(3000, () => {
+  console.log("Listening at :3000...");
+});
