3bbd82a9c42bbe55d751a62391135fef85850379
Aldo Pizarro <a.pizarro339@gmail.com>
2019-07-09 15:48:08 -0400

    asd

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-Mongoose.connect("mongodb://172.30.136.250:27017/local");
+Mongoose.connect("mongodb://172.30.136.250:27017/dbexample");
 
 const PersonModel = Mongoose.model("personas", {
   firstname: String,
